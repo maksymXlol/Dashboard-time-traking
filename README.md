f3cfc9b6f0f4ee75b8b5eace79e7c36121fc65a3
maksymXlol <124809697+maksymXlol@users.noreply.github.com>
2023-07-23 08:28:12 +0100

    finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Frontend Mentor - Time tracking dashboard
+
+[ _Hello this is my time traking dashboard:_](https://maksymxlol.github.io/Dashboard-time-traking/)
--- script.js
+++ script.js
@@ -3,10 +3,33 @@ import { data } from "./data.js";
 document.addEventListener("click", click);
 function click(event) {
   if (event.target.classList.contains("filter__input")) {
-    console.log(event.target);
+    let frame = event.target.value;
+    // console.log(data[0].timeframes[frame], frame);
+    setTime(frame);
   }
 }
 
-for (let i = 0; i < data.length; i++) {
-  console.log(data[i].title);
+function setTime(frame) {
+  for (let i = 0; i < data.length; i++) {
+    let title = data[i].title;
+    let element = document.querySelector(`.info[data-info='${title}']`);
+    let current = element.querySelector(".time");
+    let previous = element.querySelector(".last");
+    let frameString =
+      frame == "daily"
+        ? "Yesterday"
+        : frame == "monthly"
+        ? "Last Month"
+        : "Last Week";
+    current.textContent = `${data[i].timeframes[frame].current}hrs`;
+    previous.textContent = `${frameString} - ${data[i].timeframes[frame].previous}hrs`;
+  }
 }
+
+setTime(document.querySelector("input[checked]").value);
+
+// for (const iterator of data) {
+//   console.log(iterator.title);
+// }
+
+// data.forEach((item, index) => console.log(item.title, index));
